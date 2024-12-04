8ce7cc1e0dc42fb5298a728b2c56b8f8fc18c9e6
Nikolas Strohecker <sixniks2@gmail.com>
2024-12-04 16:53:56 -0700

    Make simple Etch-A-Sketch

---
+++ script.js
@@ -0,0 +1,59 @@
+//Create a grid with a fixed width
+//Each piece of grid should resize to take available space
+
+
+
+const goButton= document.querySelector("button")
+const cont= document.querySelector(".container")
+let userInput= document.querySelector("input")
+
+let userInputNum=Number(userInput)
+
+goButton.addEventListener("click",getInput)
+function getInput(){
+    cont.innerText=""
+   let number=Number(userInput.value)
+if(number>=96){
+    userInput.value=""
+    return alert("ERROR please enter a number under 96")
+}
+    userInput.value=""
+
+    for(i=number; i>0; i--){
+       
+        let newBoxH=document.createElement("div")
+        newBoxH.className="newBoxH"
+        cont.appendChild(newBoxH)
+         }
+         let boxHover=document.querySelectorAll(".newBoxH")
+         boxHover.forEach(element => {
+            element.addEventListener("mouseenter", changeBackgroundColor );
+            let intCount=0
+            let opacity= 0
+          
+            function changeBackgroundColor(){
+                
+                intCount+=1
+                console.log(intCount)
+                
+                
+                
+                
+                element.style.opacity=opacity+=.10
+                console.log(opacity)
+                
+                
+                 element.style.backgroundColor="rgb("+Math.random()*255+","+Math.random()*255+", "+Math.random()*255+")"
+                 
+                //  console.log(opacity)
+                }
+            userInput.value=""
+         });
+         number=0
+}
+
+
+
+
+
+
